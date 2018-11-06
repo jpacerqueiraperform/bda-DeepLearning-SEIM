835c6b13e92e33b6f0b3cd8b80c339883802d2b4
jpacerqueiraperform <joao.cerqueira@performgroup.com>
2018-11-06 15:18:34 +0000

    Baseline phishtank-learning-model

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+   New SEIM project for DAZN Group
+
+
+    - On-premise work for DeepLearning 
+
+      - Setup of H2O Deep Learning
+         - AUTOML for H2o
+
+      - Setup of Keras for Python 3 - Anaconda 5.1.0.1
+
+
+
---
+++ phishtank-learning-model/get-physhtank-db.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# USING OPTION : phishtank
+
+wget http://data.phishtank.com/data/online-valid.json.bz2 
+wget http://data.phishtank.com/data/online-valid.csv.bz2 
+mv online-valid.json.bz2 seim/
+mv online-valid.csv.bz2 seim/
+bzip2 -dk seim/online-valid.json.bz2
+bzip2 -dk seim/online-valid.csv.bz2
+sed -i 's/,yes,/,1,/g' seim/online-valid.csv
+sed -i 's/,no,/,0,/g' seim/online-valid.csv
+
+echo "Done!"
