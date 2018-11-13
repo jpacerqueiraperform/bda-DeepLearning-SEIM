adc760399d6fba0ab526566579520132e6d29a81
jpacerqueiraperform <joao.cerqueira@performgroup.com>
2018-11-13 17:12:58 +0000

    commoncrawl with more sources

--- phishtank-commoncrawl-learning-model/clean-json-commomcrawl-url.sh
+++ phishtank-commoncrawl-learning-model/clean-json-commomcrawl-url.sh
@@ -2,19 +2,13 @@
 # USING OPTION : phishtank
 
 echo "Follow latest archive here : http://commoncrawl.org/2018/10/october-2018-crawl-archive-now-available/ "
-echo "List of Top Level Domains : *.com *.net *.org *.co.uk *.ru "
+echo "List of Top Level Domains : *.com *.org *.co.uk *.ru && *.net *.cn *.cz *.kp *.us "
 
 export FILELOCATION=$PWD
 rm -rf siem/clean-CC-MAIN-2018-43-index-.*.json
 hdfs dfs -mkdir -p data/raw/commoncrawl/dt=20181112
 
 export FNAME=CC-MAIN-2018-43-index-.com.json
-sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
-tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
-mv clean-$FNAME siem/
-hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
-
-export FNAME=CC-MAIN-2018-43-index-.net.json
 sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
 tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
 mv clean-$FNAME siem/
@@ -33,6 +27,36 @@ mv clean-$FNAME siem/
 hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
 
 export FNAME=CC-MAIN-2018-43-index-.ru.json
+sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
+tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
+mv clean-$FNAME siem/
+hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+
+export FNAME=CC-MAIN-2018-43-index-.net.json
+sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
+tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
+mv clean-$FNAME siem/
+hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+
+export FNAME=CC-MAIN-2018-43-index-.cn.json
+sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
+tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
+mv clean-$FNAME siem/
+hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+
+export FNAME=CC-MAIN-2018-43-index-.cz.json
+sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
+tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
+mv clean-$FNAME siem/
+hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+
+export FNAME=CC-MAIN-2018-43-index-.kp.json
+sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
+tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
+mv clean-$FNAME siem/
+hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+
+export FNAME=CC-MAIN-2018-43-index-.us.json
 sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
 tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
 mv clean-$FNAME siem/
--- phishtank-commoncrawl-learning-model/get-commomcrawl-url.sh
+++ phishtank-commoncrawl-learning-model/get-commomcrawl-url.sh
@@ -2,7 +2,7 @@
 # USING OPTION : commomcrawl 
 
 echo "Follow latest archive here : http://commoncrawl.org/2018/10/october-2018-crawl-archive-now-available/ "
-echo "List of Top Level Domains : *.com *.net *.org *.co.uk *.ru "
+echo "List of Top Level Domains : *.com *.org *.co.uk *.ru *.net *.cn *.cz *.kp *.us "
 
 rm CC-MAIN-2018-43-index?url=*.*
 
@@ -10,10 +10,15 @@ export http_proxy="http://proxy:3128"
 export https_proxy="https://proxy:3128"
 
 wget -O  CC-MAIN-2018-43-index-.com.json  https://index.commoncrawl.org/CC-MAIN-2018-43-index?url=*.com&output=json
-wget -O  CC-MAIN-2018-43-index-.net.json  https://index.commoncrawl.org/CC-MAIN-2018-43-index?url=*.net&output=json
 wget -O  CC-MAIN-2018-43-index-.org.json  https://index.commoncrawl.org/CC-MAIN-2018-43-index?url=*.org&output=json
 wget -O  CC-MAIN-2018-43-index-.co.uk.json  https://index.commoncrawl.org/CC-MAIN-2018-43-index?url=*.co.uk&output=json
 wget -O  CC-MAIN-2018-43-index-.ru.json  https://index.commoncrawl.org/CC-MAIN-2018-43-index?url=*.ru&output=json
+
+wget -O  CC-MAIN-2018-43-index-.net.json  https://index.commoncrawl.org/CC-MAIN-2018-43-index?url=*.net&output=json
+wget -O  CC-MAIN-2018-43-index-.cn.json  https://index.commoncrawl.org/CC-MAIN-2018-43-index?url=*.cn&output=json
+wget -O  CC-MAIN-2018-43-index-.cz.json  https://index.commoncrawl.org/CC-MAIN-2018-43-index?url=*.cz&output=json
+wget -O  CC-MAIN-2018-43-index-.kp.json  https://index.commoncrawl.org/CC-MAIN-2018-43-index?url=*.kp&output=json
+wget -O  CC-MAIN-2018-43-index-.us.json  https://index.commoncrawl.org/CC-MAIN-2018-43-index?url=*.us&output=json
 
 # unset http_proxy
 # unset https_proxy
