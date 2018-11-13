e3b55318603436672fe88aaea94e81073bb111b0
jpacerqueiraperform <joao.cerqueira@performgroup.com>
2018-11-13 11:40:53 +0000

    New versions for phishtank-commoncrawl-learning-model

--- phishtank-commoncrawl-learning-model/clean-phishtank-db.sh
+++ phishtank-commoncrawl-learning-model/clean-phishtank-db.sh
@@ -8,3 +8,5 @@ bzip2 -dk siem/verified_online.csv.bz2
 sed -i 's/,yes,/,1,/g' siem/verified_online.csv
 sed -i 's/,no,/,0,/g' siem/verified_online.csv
 
+hdfs dfs -copyFromLocal -f siem/verified_online.json data/raw/phishtank/dt=20181112
+hdfs dfs -copyFromLocal -f siem/verified_online.csv data/raw/phishtank/dt=20181112
