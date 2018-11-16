f63720db13442ac71ff4661444aad05ca6bcf620
jpacerqueiraperform <joao.cerqueira@performgroup.com>
2018-11-16 21:19:30 +0000

    Improved datacleanse scripts

--- phishtank-commoncrawl-learning-model/clean-json-commomcrawl-url.sh
+++ phishtank-commoncrawl-learning-model/clean-json-commomcrawl-url.sh
@@ -11,55 +11,55 @@ hdfs dfs -mkdir -p data/raw/commoncrawl/dt=20181112
 export FNAME=CC-MAIN-2018-43-index-.com.json
 sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
 tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
-mv clean-$FNAME siem/
-hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+mv clean-$FNAME siem/clean_data/
+hdfs dfs -copyFromLocal -f siem/clean_data/clean-$FNAME data/raw/commoncrawl/dt=20181112
 
 export FNAME=CC-MAIN-2018-43-index-.org.json
 sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
 tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
-mv clean-$FNAME siem/
-hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+mv clean-$FNAME siem/clean_data/
+hdfs dfs -copyFromLocal -f siem/clean_data/clean-$FNAME data/raw/commoncrawl/dt=20181112
 
 export FNAME=CC-MAIN-2018-43-index-.co.uk.json
 sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
 tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
-mv clean-$FNAME siem/
-hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+mv clean-$FNAME siem/clean_data/
+hdfs dfs -copyFromLocal -f siem/clean_data/clean-$FNAME data/raw/commoncrawl/dt=20181112
 
 export FNAME=CC-MAIN-2018-43-index-.ru.json
 sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
 tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
-mv clean-$FNAME siem/
-hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+mv clean-$FNAME siem/clean_data/
+hdfs dfs -copyFromLocal -f siem/clean_data/clean-$FNAME data/raw/commoncrawl/dt=20181112
 
 export FNAME=CC-MAIN-2018-43-index-.net.json
 sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
 tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
-mv clean-$FNAME siem/
-hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+mv clean-$FNAME siem/clean_data/
+hdfs dfs -copyFromLocal -f siem/clean_data/clean-$FNAME data/raw/commoncrawl/dt=20181112
 
 export FNAME=CC-MAIN-2018-43-index-.cn.json
 sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
 tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
-mv clean-$FNAME siem/
-hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+mv clean-$FNAME siem/clean_data/
+hdfs dfs -copyFromLocal -f siem/clean_data/clean-$FNAME data/raw/commoncrawl/dt=20181112
 
 export FNAME=CC-MAIN-2018-43-index-.cz.json
 sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
 tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
-mv clean-$FNAME siem/
-hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+mv clean-$FNAME siem/clean_data/
+hdfs dfs -copyFromLocal -f siem/clean_data/clean-$FNAME data/raw/commoncrawl/dt=20181112
 
 export FNAME=CC-MAIN-2018-43-index-.kp.json
 sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
-tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
+tail -n 1 "$FILELOCATION/clean_data/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
 mv clean-$FNAME siem/
-hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+hdfs dfs -copyFromLocal -f siem/clean_data/clean-$FNAME data/raw/commoncrawl/dt=20181112
 
 export FNAME=CC-MAIN-2018-43-index-.us.json
 sed -n -e 's/^.*\({"url":\)/\1/p' $FNAME >> clean-$FNAME
 tail -n 1 "$FILELOCATION/clean-$FNAME" | wc -c | xargs -I {} truncate "$FILELOCATION/clean-$FNAME" -s -{}
-mv clean-$FNAME siem/
-hdfs dfs -copyFromLocal -f siem/clean-$FNAME data/raw/commoncrawl/dt=20181112
+mv clean-$FNAME siem/clean_data/
+hdfs dfs -copyFromLocal -f siem/clean_data/clean-$FNAME data/raw/commoncrawl/dt=20181112
 
 echo "Done!"
--- phishtank-commoncrawl-learning-model/clean-phishtank-db.sh
+++ phishtank-commoncrawl-learning-model/clean-phishtank-db.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # USING OPTION : phishtank
 
-mv verified_online.json.bz2 siem/
-mv verified_online.csv.bz2 siem/
-bzip2 -dk siem/verified_online.json.bz2
-bzip2 -dk siem/verified_online.csv.bz2
-sed -i 's/,yes,/,1,/g' siem/verified_online.csv
-sed -i 's/,no,/,0,/g' siem/verified_online.csv
+mv verified_online.json.bz2 siem/clean_data/
+mv verified_online.csv.bz2 siem/clean_data/
+bzip2 -dk siem/clean_data/verified_online.json.bz2
+bzip2 -dk siem/clean_data/verified_online.csv.bz2
+sed -i 's/,yes,/,1,/g' siem/clean_data/verified_online.csv
+sed -i 's/,no,/,0,/g' siem/clean_data/verified_online.csv
 
-hdfs dfs -copyFromLocal -f siem/verified_online.json data/raw/phishtank/dt=20181112
-hdfs dfs -copyFromLocal -f siem/verified_online.csv data/raw/phishtank/dt=20181112
+hdfs dfs -copyFromLocal -f siem/clean_data/verified_online.json data/raw/phishtank/dt=20181112
+hdfs dfs -copyFromLocal -f siem/clean_data/verified_online.csv data/raw/phishtank/dt=20181112
