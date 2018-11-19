062c85e096eae4f99af33b6155b3174b928d37b1
jpacerqueiraperform <joao.cerqueira@performgroup.com>
2018-11-19 10:54:21 +0000

    Cast Variables for type infer

--- notebooks/siem/pyspark_clean_data/import-to-urlpredictions.py
+++ notebooks/siem/pyspark_clean_data/import-to-urlpredictions.py
@@ -12,16 +12,13 @@ sparkSession = (SparkSession
 process_date='20181116'
 input_file="hdfs:///user/siemanalyst/data/staged/urltopredict/dt="+process_date
 #
-#
-url_toprd_df= sparkSession.read.json(input_file)
-url_toprd_df.printSchema()
-url_toprd_df.registerTempTable("urltopredict")
 
-# HiveQL Scoring table Load
+# SparkSQL read files as temp_table 
+sql_read_files ="SELECT CAST(urltopredict_files.url AS STRING) AS url, CAST(urltopredict_files.ynverified  AS INT) AS ynverified , CAST(urltopredict_files.url_length  AS INT) AS url_length, CAST(urltopredict_files.massiveurl  AS INT) AS massiveurl ,CAST(urltopredict_files.count_at  AS INT) AS count_at , CAST(urltopredict_files.count_dot  AS INT) AS count_dot, CAST(urltopredict_files.url_is_ip  AS INT) AS url_is_ip, CAST(urltopredict_files.count_dot_com  AS INT) AS count_dot_com, CAST(urltopredict_files.url_kl_en AS DOUBLE) AS url_kl_en, CAST(urltopredict_files.url_bad_kl_en AS INT) AS url_bad_kl_en ,CAST(urltopredict_files.url_ks_en AS DOUBLE) AS url_ks_en ,CAST(urltopredict_files.url_bad_ks_en AS INT) AS url_bad_ks_en FROM urltopredict_files"
+# HiveQL Scoring table Load
 sql_publish = "INSERT INTO TABLE siem.url_mdl_score_predict PARTITION (dt="+process_date+")"+\
-" SELECT CAST(urltopredict.url AS STRING) AS url, CAST(urltopredict.ynverified  AS INT) AS ynverified , CAST(urltopredict.url_length  AS INT) AS url_length, CAST(urltopredict.massiveurl  AS INT) AS massiveurl ,CAST(urltopredict.count_at  AS INT) AS count_at ,CAST(urltopredict.count_dot  AS INT) AS count_dot, CAST(urltopredict.url_is_ip  AS INT) AS url_is_ip, CAST(urltopredict.count_dot_com  AS INT) AS count_dot_com, CAST(urltopredict.url_kl_en AS DOUBLE) AS url_kl_en, CAST(urltopredict.url_bad_kl_en AS INT) AS url_bad_kl_en ,CAST(urltopredict.url_ks_en AS DOUBLE) AS url_ks_en ,CAST(urltopredict.url_bad_ks_en AS INT) AS url_bad_ks_en, scoredatav2(url_length,massiveurl,count_at,count_dot,url_is_ip,count_dot_com,url_kl_en,url_bad_kl_en,url_ks_en,url_bad_ks_en) as mdl_score_phishing FROM urltopredict"
+" SELECT  url, ynverified, url_length, massiveurl, count_at, count_dot, url_is_ip, count_dot_com, url_kl_en, url_bad_kl_en, url_ks_en, url_bad_ks_en, scoredatav2(url_length, massiveurl, count_at, count_dot, url_is_ip, count_dot_com, url_kl_en, url_bad_kl_en, url_ks_en, url_bad_ks_en) as mdl_score_phishing FROM urltopredict"
 
-#scoredatav2(CAST(urltopredict.url_length  AS INT),CAST(urltopredict.massiveurl  AS INT) AS massiveurl,CAST(urltopredict.count_at  AS INT) AS count_at ,CAST(urltopredict.count_dot  AS INT) AS count_dot, CAST(urltopredict.url_is_ip  AS INT) AS url_is_ip, CAST(urltopredict.count_dot_com  AS INT) AS count_dot_com, CAST(urltopredict.url_kl_en AS DOUBLE) AS url_kl_en, CAST(urltopredict.url_bad_kl_en AS INT) AS url_bad_kl_en ,CAST(urltopredict.url_ks_en AS DOUBLE) AS url_ks_en ,CAST(urltopredict.url_bad_ks_en AS INT) as mdl_score_phishing 
 
 # scoredatav2 requires: [url_length, massiveurl, count_at, count_dot, url_is_ip, count_dot_com, url_kl_en, url_bad_kl_en, url_ks_en, url_bad_ks_en, ynverified]
 
@@ -30,11 +27,19 @@ sql_publish = "INSERT INTO TABLE siem.url_mdl_score_predict PARTITION (dt="+proc
 sparkSession.sql("ADD JAR hdfs:////user/siemanalyst/predictor/udf/StackedEnsemble_AllModels_AutoML/h2o-genmodel.jar")
 sparkSession.sql("ADD JAR hdfs:///user/siemanalyst/predictor/udf/StackedEnsemble_AllModels_AutoML/ScoreDataUDFAUTOML-2.0-SNAPSHOT.jar")
 sparkSession.sql("CREATE TEMPORARY FUNCTION scoredatav2 AS 'ai.h2o.hive.udf.ScoreDataM2UDF' ")
-
 sparkSession.sql("ALTER TABLE siem.url_mdl_score_predict DROP IF EXISTS PARTITION (dt="+process_date+")")
 
+# Spark read Source Files
+#
+url_toprd_df= sparkSession.read.json(input_file)
+url_toprd_df.printSchema()
+url_toprd_df.registerTempTable("urltopredict_files")
+#
+# Read Cast Variables
+df_cast_structure=sparkSession.sql(sql_read_files)
+df_cast_structure.registerTempTable("urltopredict")
+#
 # Write into Hive
 df_load = sparkSession.sql(sql_publish)
-
+#
 sparkSession.stop()
-                
--- notebooks/siem/pyspark_clean_data/run-pyspark2-cleandata.sh
+++ notebooks/siem/pyspark_clean_data/run-pyspark2-cleandata.sh
@@ -1,3 +1,7 @@
 #spark2-submit --master yarn --deploy-mode client cleandata.py
 #spark2-submit --master yarn --deploy-mode client  import-to-urltopredict.py
-spark2-submit --master yarn --deploy-mode client --jars $PWD/spark_jars/ScoreDataUDFAUTOML-2.0-SNAPSHOT.jar,$PWD/spark_jars/h2o-genmodel.jar  import-to-urlpredictions.py
+#spark2-submit --master yarn --deploy-mode client --jars $PWD/spark_jars/ScoreDataUDFAUTOML-2.0-SNAPSHOT.jar,$PWD/spark_jars/h2o-genmodel.jar  import-to-urlpredictions.py
+MY_FOLDER="/home/siemanalyst/notebooks/siem/pyspark_clean_data"
+
+spark2-submit --master yarn --deploy-mode client $MY_FOLDER/cleandata.py
+spark2-submit --master yarn --deploy-mode client --jars $MY_FOLDER/spark_jars/ScoreDataUDFAUTOML-2.0-SNAPSHOT.jar,$MY_FOLDER/spark_jars/h2o-genmodel.jar  $MY_FOLDER/import-to-urlpredictions.py
