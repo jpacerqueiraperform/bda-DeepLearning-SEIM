d690a777769d8c4c887cfe32ee1a8471bc823690
jpacerqueiraperform <joao.cerqueira@performgroup.com>
2018-11-23 20:27:10 +0000

    Log of workflows into the Model3 scripting process

--- notebooks/siem/pyspark_clean_data/run-pyspark2-cleandata.sh
+++ notebooks/siem/pyspark_clean_data/run-pyspark2-cleandata.sh
@@ -7,7 +7,7 @@ MY_FOLDER="/home/siemanalyst/notebooks/siem/pyspark_clean_data"
 #
 ### MODEL:3
 ## Stage urltopredict folder
-spark2-submit --master yarn --deploy-mode client $MY_FOLDER/cleandata-model3-func15.py
+spark2-submit --master yarn --deploy-mode client $MY_FOLDER/cleandata-model3-func15.py > $MY_FOLDER/model3-cleandata-lastrun.log
 ## Stage url_ml_score_predict
-spark2-submit --master yarn --deploy-mode client --jars $MY_FOLDER/spark_jars/ScoreDataUDFAUTOML-3.0-SNAPSHOT.jar,$MY_FOLDER/spark_jars/h2o-genmodel.jar  $MY_FOLDER/import-to-urlpredictions-model3-func15.py
+spark2-submit --master yarn --deploy-mode client --jars $MY_FOLDER/spark_jars/ScoreDataUDFAUTOML-3.0-SNAPSHOT.jar,$MY_FOLDER/spark_jars/h2o-genmodel.jar  $MY_FOLDER/import-to-urlpredictions-model3-func15.py >> $MY_FOLDER/model3-cleandata-lastrun.log
 #
--- notebooks/siem/pyspark_model_data/run-pyspark2-model3-data.sh
+++ notebooks/siem/pyspark_model_data/run-pyspark2-model3-data.sh
@@ -3,8 +3,8 @@ MY_FOLDER="/home/siemanalyst/notebooks/siem/pyspark_model_data"
 #
 ### MODEL:3
 ## Stage urltopredict folder
-spark2-submit --master yarn --deploy-mode client $MY_FOLDER/version3-phishingURL-Clean-AUTOML-V3-BestModel-Discovery.py
+spark2-submit --master yarn --deploy-mode client $MY_FOLDER/version3-phishingURL-Clean-AUTOML-V3-BestModel-Discovery.py > $MY_FOLDER/ModelBuild-AUTOML-V3-LastRun.log
 ## Stage url_ml_score_predict
 rm -rf /home/siemanalyst/notebooks/siem/product_model_bin/m40/v3/*
-spark2-submit --master yarn --deploy-mode client $MY_FOLDER/version3-phishingURL-ModelBuild-AUTOML-V3-BestModel-Discovery.py
+spark2-submit --master yarn --deploy-mode client $MY_FOLDER/version3-phishingURL-ModelBuild-AUTOML-V3-BestModel-Discovery.py  >> $MY_FOLDER/ModelBuild-AUTOML-V3-LastRun.log
 #
