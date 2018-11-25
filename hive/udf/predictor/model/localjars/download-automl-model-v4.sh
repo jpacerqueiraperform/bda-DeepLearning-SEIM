17d79aa1aa01ea67005059e7175525319ab3d308
jpacerqueiraperform <joao.cerqueira@performgroup.com>
2018-11-25 01:30:00 +0000

    New Model4 Prediction Tables and results

--- hive/udf/predictor/model/localjars/download-automl-model-v4.sh
+++ hive/udf/predictor/model/localjars/download-automl-model-v4.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
 scp siemanalyst@chpbdaodi02.prod.ch.perform.local:~/notebooks/siem/product_model_bin/m40/v4/mojo/h2o-genmodel.jar .
-scp siemanalyst@chpbdaodi02.prod.ch.perform.local:~/notebooks/siem/product_model_bin/m40/v4/mojo/*_AutoML_*_*.zip .
-cp *_AutoML_*_*.zip ../src/main/resources/ai/h2o/hive/udf/
+scp siemanalyst@chpbdaodi02.prod.ch.perform.local:~/notebooks/siem/product_model_bin/m40/v4/mojo/DRF_1_AutoML_20181124_233356.zip .
+cp DRF_1_AutoML_20181124_233356.zip ../src/main/resources/ai/h2o/hive/udf/
 #
--- notebooks/siem/pyspark_clean_data/run-pyspark2-cleandata-model4.sh
+++ notebooks/siem/pyspark_clean_data/run-pyspark2-cleandata-model4.sh
@@ -5,5 +5,5 @@ MY_FOLDER="/home/siemanalyst/notebooks/siem/pyspark_clean_data"
 ## Stage urltopredict folder
 spark2-submit --master yarn --deploy-mode client $MY_FOLDER/cleandata-model4-19functions.py > $MY_FOLDER/model4-cleandata-lastrun.log
 ## Stage url_ml_score_predict
-spark2-submit --master yarn --deploy-mode client --jars $MY_FOLDER/spark_jars/ScoreDataUDFAUTOML-4.0-SNAPSHOT.jar,$MY_FOLDER/spark_jars/h2o-genmodel.jar  $MY_FOLDER/import-to-Table-model4-19functions.py >> $MY_FOLDER/model3-cleandata-lastrun.log
+spark2-submit --master yarn --deploy-mode client --jars $MY_FOLDER/spark_jars/ScoreDataUDFAUTOML-4.0-SNAPSHOT.jar,$MY_FOLDER/spark_jars/h2o-genmodel.jar  $MY_FOLDER/import-TableAs-model4-19functions.py >> $MY_FOLDER/model4-cleandata-lastrun.log
 #
