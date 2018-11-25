cf316dbbbe0c27fe8267517e85e2759c148ec39a
jpacerqueiraperform <joao.cerqueira@performgroup.com>
2018-11-25 00:51:22 +0000

    New Template Format for Version4

--- template_model_cleandata/pyspark_execute/run-pyspark2-model4-data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-MY_FOLDER="/home/siemanalyst/notebooks/siem/pyspark_model_data"
-#
-### MODEL:3
-## Stage urltopredict folder
-spark2-submit --master yarn --deploy-mode client $MY_FOLDER/version4-phishingURL-Clean-AUTOML-V4-BestModel-Discovery.py
-## Stage url_ml_score_predict
-spark2-submit --master yarn --deploy-mode client $MY_FOLDER/version4-phishingURL-ModelBuild-AUTOML-V4-BestModel-Discovery.py
-#
