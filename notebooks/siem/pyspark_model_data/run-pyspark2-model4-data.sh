9d63af87774ef12c43b39803630f74990afba297
jpacerqueiraperform <joao.cerqueira@performgroup.com>
2018-11-29 14:44:41 +0000

    Model with proxy unset

--- notebooks/siem/pyspark_model_data/run-pyspark2-model4-data.sh
+++ notebooks/siem/pyspark_model_data/run-pyspark2-model4-data.sh
@@ -2,7 +2,8 @@
 MY_FOLDER="/home/siemanalyst/notebooks/siem/pyspark_model_data"
 #
 ### MODEL:4
-## Stage urltopredict folder 
+## Stage urltopredict folder
+unset http_proxy
 spark2-submit --master yarn --deploy-mode client $MY_FOLDER/version4-phishingURL-Clean-AUTOML-V4-BestModel-Discovery.py > $MY_FOLDER/ModelBuild-AUTOML-V4-LastRun.log
 ## Rebuild Model url_ml_score_predict under AutoMl 25Models
 rm -rf /home/siemanalyst/notebooks/siem/product_model_bin/m25/v4/*
